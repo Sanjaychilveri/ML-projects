f659bec9f3ef5b89ddacb2b874ae0f62462e60bb
Sanjaychilveri <chilverisanjay@gmail.com>
2024-06-11 01:49:39 +0530

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 # building own custom exception handlers
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -22,11 +22,11 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-# if __name__=="__main__":
-#     try:
-#         a=1/0
+if __name__=="__main__":
+    try:
+        a=1/0
 
-#     except Exception as e:
-#         logging.info("Divide by zero")
-#         raise CustomException(e,sys)
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomException(e,sys)
         
